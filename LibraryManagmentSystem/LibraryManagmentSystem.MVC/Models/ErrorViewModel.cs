8db2f9cc8b394b700d27f02d0abb98be1df284b1
Awahir <matuszkamil076@gmail.com>
2023-03-26 22:24:01 +0200

    Initial commit

---
+++ LibraryManagmentSystem/LibraryManagmentSystem.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryManagmentSystem.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
