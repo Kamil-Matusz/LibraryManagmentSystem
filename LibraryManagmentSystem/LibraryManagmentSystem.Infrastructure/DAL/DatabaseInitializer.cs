8602041cbb3e4a334ee7473717ea20187766e60d
Awahir <matuszkamil076@gmail.com>
2023-03-29 21:16:49 +0200

    Auto migration % seeding data

--- LibraryManagmentSystem/LibraryManagmentSystem.Infrastructure/DAL/DatabaseInitializer.cs
+++ LibraryManagmentSystem/LibraryManagmentSystem.Infrastructure/DAL/DatabaseInitializer.cs
@@ -1,28 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿namespace LibraryManagmentSystem.Infrastructure.DAL;
 
-namespace LibraryManagmentSystem.Infrastructure.DAL;
-
-internal sealed class DatabaseInitializer : IHostedService
+public class DatabaseInitializer
 {
-    private readonly IServiceProvider _serviceProvider;
-
-    public DatabaseInitializer(IServiceProvider serviceProvider)
-    {
-        _serviceProvider = serviceProvider;
-    }
-
-    public Task StartAsync(CancellationToken cancellationToken)
-    {
-        using (var scope = _serviceProvider.CreateScope())
-        {
-            var dbContext = scope.ServiceProvider.GetRequiredService<LibraryDbContext>();
-            dbContext.Database.Migrate();
-        }
-        
-        return Task.CompletedTask;
-    }
-
-    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    
 }
