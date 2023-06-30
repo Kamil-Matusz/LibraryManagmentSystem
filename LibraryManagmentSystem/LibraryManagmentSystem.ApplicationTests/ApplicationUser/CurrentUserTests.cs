e671dcfa00ad0de0ac5238ea2d16b0f56dd36e33
Awahir <matuszkamil076@gmail.com>
2023-06-30 21:31:15 +0200

    Application Tests

---
+++ LibraryManagmentSystem/LibraryManagmentSystem.ApplicationTests/ApplicationUser/CurrentUserTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using LibraryManagmentSystem.Application.ApplicationUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LibraryManagmentSystem.Application.ApplicationUser.Tests
+{
+    public class CurrentUserTests
+    {
+        [Fact()]
+        public void IsInRole_ShouldReturnTrue()
+        {
+            // arrange
+            var currentUser = new CurrentUser("1","admin@test.com",new List<string> {"Admin","User"});
+
+            // act
+            var isInRole = currentUser.IsInRole("Admin");
+
+            // assert
+
+            isInRole.Should().BeTrue();
+        }
+    }
+}
---
+++ LibraryManagmentSystem/LibraryManagmentSystem.ApplicationTests/Commands/Handlers/DeleteBookHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using LibraryManagmentSystem.Application.Commands.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryManagmentSystem.Domain.Interfaces;
+using Moq;
+
+namespace LibraryManagmentSystem.Application.Commands.Handlers.Tests
+{
+    public class DeleteBookHandlerTests
+    {
+        [Fact]
+        public async Task Handle_DeletesBook()
+        {
+            // arrange
+            var booksRepositoryMock = new Mock<IBooksRepository>();
+            var handler = new DeleteBookHandler(booksRepositoryMock.Object);
+            var command = new DeleteBookCommand { Name = "TestBook" };
+            CancellationToken cancellationToken = default;
+
+            // act
+            await handler.Handle(command, cancellationToken);
+
+            // assert
+            booksRepositoryMock.Verify(r => r.DeleteBook(command.Name), Times.Once);
+        }
+    }
+}
